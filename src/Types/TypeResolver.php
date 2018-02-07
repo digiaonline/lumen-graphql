b1ee5dfc86a6403cf972444137f18e7ec8513eda
Hung Neo <hungnq1989@gmail.com>
2018-02-07 23:17:27 +0200

    Add default TypeResolver

---
+++ src/Types/TypeResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Digia\Lumen\GraphQL\Types;
+
+use Digia\Lumen\GraphQL\Exceptions\UnsupportedTypeException;
+use Youshido\GraphQL\Type\AbstractType;
+
+/**
+ * Class TypeResolver
+ * @package Digia\Lumen\GraphQL\Types
+ */
+class TypeResolver
+{
+    /**
+     * @param array $object
+     *
+     * @return AbstractType
+     *
+     * @throws UnsupportedTypeException
+     */
+    public static function resolveType(array $object): AbstractType
+    {
+        if (!array_key_exists($object['type'], config('graphql.types'))) {
+            throw new UnsupportedTypeException(sprintf('Unsupported entity type `%s`.', $object['type']));
+        }
+
+        $className = config('graphql.types')[$object['type']];
+
+        return new $className();
+    }
+}
